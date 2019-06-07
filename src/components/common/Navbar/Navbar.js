11e939febf116c47a4955c9072e369857ef651f5
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2019-06-07 22:30:58 +0200

    Remove brands

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import { Nav, NavItem, Brand, StyledContainer, NavListWrapper, MobileMenu, Mobil
 
 import { ReactComponent as MenuIcon } from '@static/icons/menu.svg';
 
-const NAV_ITEMS = ['About', 'Brands', 'Team', 'FAQ'];
+const NAV_ITEMS = ['About', 'Team', 'FAQ'];
 
 class Navbar extends Component {
   state = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Navbar from '@common/Navbar';
 
 import Header from '@sections/Header';
 import About from '@sections/About';
-import Brands from '@sections/Brands';
 import Team from '@sections/Team';
 import Faq from '@sections/Faq';
 import Footer from '@sections/Footer';
@@ -15,7 +14,6 @@ const IndexPage = () => (
     <Navbar />
     <Header />
     <About />
-    <Brands />
     <Team />
     <Faq />
     <Footer />
