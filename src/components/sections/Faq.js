00cb4978b68ca308c3809118377dd0f393c3c605
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2019-08-05 18:08:06 +0200

    Update format

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -10,11 +10,11 @@ const FAQS = [
     title: 'Quién ideó y quién financia este proyecto?',
     content: () => (
       <>
-        Ágora 2 es una idea de que nace de un debate entre sus fundadores, ciudadanos cubanos, sin mediación, 
-      motivación o injerencia de ninguna otra instancia, nacional o extranjera. Su financiamiento provendrá 
-      exclusivamente de ciudadanos cubanos con derecho al voto, y el dinero se usará para pagar los servicios 
-      de la web. El equipo trabajará siempre de forma voluntaria y no remunerada.
-
+        Ágora 2 es una idea de que nace de un debate entre sus fundadores, ciudadanos cubanos, sin
+        mediación, motivación o injerencia de ninguna otra instancia, nacional o extranjera. Su
+        financiamiento provendrá exclusivamente de ciudadanos cubanos con derecho al voto, y el
+        dinero se usará para pagar los servicios de la web. El equipo trabajará siempre de forma
+        voluntaria y no remunerada.
       </>
     )
   },
@@ -22,11 +22,12 @@ const FAQS = [
     title: 'Cómo puedo ayudar?',
     content: () => (
       <>
-        Hay tres formas fundamentales de ayudar al proyecto. La primera es como miembro del equipo. Para eso 
-      puede enviarnos su disposición via email a la dirección admin@agora2.org o contactar directamente a un 
-      miembro del equipo. La segunda es contribuyendo con la divulgación del proyecto a nivel comunitario para 
-      motivar la participación popular en los debates de Ágora. Y la tercera es haciendo una donación a la cuenta 
-      del proyecto (esta cuenta todavía no existe), que se usará para pagar los servicios de hosting del sitio web.
+        Hay tres formas fundamentales de ayudar al proyecto. La primera es como miembro del equipo.
+        Para eso puede enviarnos su disposición via email a la dirección admin@agora2.org o
+        contactar directamente a un miembro del equipo. La segunda es contribuyendo con la
+        divulgación del proyecto a nivel comunitario para motivar la participación popular en los
+        debates de Ágora. Y la tercera es haciendo una donación a la cuenta del proyecto (esta
+        cuenta todavía no existe), que se usará para pagar los servicios de hosting del sitio web.
       </>
     )
   },
@@ -34,11 +35,11 @@ const FAQS = [
     title: 'Qué filiación política tiene Ágora 2?',
     content: () => (
       <>
-        Ágora 2 se esforzará en no preferenciar ninguna inclinación política más allá de la observancia 
-      estricta de la constitución de la República de Cuba. Como está recogido en nuestro código de conducta, 
-      cada miembro del equipo se compromete a que sus sesgos políticos individuales no interfieran con los 
-      objetios democráticos del proyecto. La meta es garantizar que todas las opiniones, expresadas adecuadamente, 
-      tengan espacio y respeto.
+        Ágora 2 se esforzará en no preferenciar ninguna inclinación política más allá de la
+        observancia estricta de la constitución de la República de Cuba. Como está recogido en
+        nuestro código de conducta, cada miembro del equipo se compromete a que sus sesgos políticos
+        individuales no interfieran con los objetios democráticos del proyecto. La meta es
+        garantizar que todas las opiniones, expresadas adecuadamente, tengan espacio y respeto.
       </>
     )
   },
@@ -58,12 +59,12 @@ const FAQS = [
     title: 'What exactly does Gatsby build?',
     content: () => (
       <>
-        La plataforma está pensada para los electores, esto es, ciudadanos cubanos con derecho al voto, 
-      ellos pueden abrir una cuenta de tipo Elector y tener acceso a todas las funcionalidades de Ágora 2. 
-      Adicionalmente, las cuentas de tipo Observador están pensadas para usuarios que, sin formar parte 
-      activa del proceso eleccionario, quieran participar observando el proceso e interviniendo en los 
-      debates. Consideramos que esta posibilidad enriquecerá las discusiones ayudando a contrastar y validar 
-      opiniones y puntos de vista.
+        La plataforma está pensada para los electores, esto es, ciudadanos cubanos con derecho al
+        voto, ellos pueden abrir una cuenta de tipo Elector y tener acceso a todas las
+        funcionalidades de Ágora 2. Adicionalmente, las cuentas de tipo Observador están pensadas
+        para usuarios que, sin formar parte activa del proceso eleccionario, quieran participar
+        observando el proceso e interviniendo en los debates. Consideramos que esta posibilidad
+        enriquecerá las discusiones ayudando a contrastar y validar opiniones y puntos de vista.
       </>
     )
   }
